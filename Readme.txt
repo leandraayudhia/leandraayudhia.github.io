eedadb1f3eca167dc23f7200c93aa3d937fd51ba
leandraayudhia <leandraayudhia@yahoo.co.id>
2021-08-17 22:38:33 +0700

    portfolio

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
---
+++ assets/file/Online-Booking-Appointment/file/script.js
@@ -0,0 +1,16 @@
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
